92c79953a8c9d89790ff660c6b68c0740281ef0f
Chris Erfort <chriserfort@Chriss-MacBook-Pro.local>
2025-07-01 13:20:50 -0400

    Edit myvault

--- Formula/myvault.rb
+++ Formula/myvault.rb
@@ -1,6 +1,6 @@
 class Myvault < Formula
   desc "Vault-related CLI tool with Touch ID auth"
-  homepage "https://github.com/yourname/myvaultcli"
+  homepage "https://github.com/cerfortrpt/myvaultcli"
   url "https://github.com/cerfortrpt/myvaultcli/releases/download/v0.1.0/vaultfingerprint"
   sha256 "paste_your_real_sha256_here"
   license "MIT"
