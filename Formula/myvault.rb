c4c0b09169c69d08d17b2d58d9bcbefd94ab3edd
Chris Erfort <chriserfort@Chriss-MacBook-Pro.local>
2025-07-01 13:13:33 -0400

    Add myvault formula

---
+++ Formula/myvault.rb
@@ -0,0 +1,15 @@
+class Myvault < Formula
+  desc "Vault-related CLI tool with Touch ID auth"
+  homepage "https://github.com/yourname/myvaultcli"
+  url "https://github.com/yourname/myvaultcli/releases/download/v0.1.0/vaultfingerprint"
+  sha256 "paste_your_real_sha256_here"
+  license "MIT"
+
+  def install
+    bin.install "vaultfingerprint" => "myvault"
+  end
+
+  test do
+    system "#{bin}/myvault", "--help"
+  end
+end
